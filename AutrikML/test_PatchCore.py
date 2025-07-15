1b7ebe455ae9b0e17ff6a1f479060ee27c53ce08
adityaguneja <101662192+adityaguneja@users.noreply.github.com>
2025-07-15 14:05:18 +0530

    Update test_PatchCore.py

--- AutrikML/test_PatchCore.py
+++ AutrikML/test_PatchCore.py
@@ -13,7 +13,7 @@ if __name__ == "__main__":
     
     datamodule = Folder(
         name="my_dataset",
-        root="data/my_dataset/test",
+        root="data/test",
         normal_dir="good",
         abnormal_dir="anomaly",
         train_batch_size=1,    
